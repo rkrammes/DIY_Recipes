eb82924bfc1fe15fe1fead24d1c67869514ef4b2
rkrammes <rkrammes@mac.com>
2025-03-27 13:47:50 -0400

    Update Supabase client to use environment variables for configuration

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -3,7 +3,7 @@
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://bzudglfxxywugesncjnz.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6dWRnbGZ4eHl3dWdlc25jam56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4Mjk5MDAsImV4cCI6MjA1NzQwNTkwMH0.yYBWuD_bzfyh72URflGqJbn-lIwrZ6oAznxVocgxOm8';
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
