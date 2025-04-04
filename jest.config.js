f4ed9ce6de81e251d1b02f56b8791c17f8337d81
rkrammes <rkrammes@mac.com>
2025-04-04 17:56:50 -0400

    .

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
-  testEnvironment: 'node',
-  moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
-  setupFiles: ['./tests/setup.js'], // Include the setup file for environment variables
-};
