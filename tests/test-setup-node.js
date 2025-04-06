895201f7e900f16b09953e215223bed7dcde08bf
rkrammes <rkrammes@mac.com>
2025-04-06 03:07:04 -0400

    l

--- tests/test-setup-node.js
+++
@@ -1,6 +0,0 @@
-// Set up Node.js environment properly for Puppeteer tests
-global.showNotification = jest.fn();
-global.alert = jest.fn();
-global.confirm = jest.fn(() => true); // Default to confirming dialogs
-
-// Other global helpers can be defined here as needed
