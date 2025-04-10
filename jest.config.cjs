f056c7afccf0a3eca49201edba3b3e795f8865b7
rkrammes <rkrammes@mac.com>
2025-04-10 08:16:47 -0400

    Update dependencies and configuration for Next.js migration

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,15 +1,19 @@
 module.exports = {
   transform: {
-    '^.+\\.js$': 'babel-jest',
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
   },
   testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   setupFilesAfterEnv: ['./tests/test-setup.js', './tests/setup.js', '@testing-library/jest-dom/extend-expect'],
-  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  testMatch: [
+    '**/__tests__/**/*.{js,ts,tsx}',
+    '**/?(*.)+(spec|test).{js,ts,tsx}'
+  ],
   verbose: true,
   collectCoverage: true,
   collectCoverageFrom: [
     'js/**/*.js',
+    'modern-diy-recipes/src/**/*.{js,ts,tsx}',
     '!**/node_modules/**'
   ],
   transformIgnorePatterns: [
