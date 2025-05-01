bb66d75550abd03fb1fc0f350019412068c0e31e
rkrammes <rkrammes@mac.com>
2025-04-30 23:56:39 -0400

    l

--- modern-diy-recipes/src/app/layout.tsx
+++ modern-diy-recipes/src/app/layout.tsx
@@ -28,12 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Suspense fallback={null}> {/* Wrap ThemeScript in Suspense */}
-        <ThemeScript />
-      </Suspense>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Suspense fallback={null}> {/* Wrap ThemeScript in Suspense */}
+          <ThemeScript />
+        </Suspense>
 
         <AuthProvider>
           <ThemeProvider>
